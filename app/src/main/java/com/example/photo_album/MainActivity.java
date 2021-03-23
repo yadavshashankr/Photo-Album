02bbe11acbbbfb6cd3f4032ea80235ef23b7a027
Shashank Yadav <yadav.shashank.r@gmail.com>
2021-03-23 13:14:48 +0530

    1:13 COMMIT

---
+++ app/src/main/java/com/example/photo_album/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.photo_album;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        //Initial Commit
+    }
+}
